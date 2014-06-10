321fe5ac14116ce07db8f9a6e82c59b6d9c4b62e
Evan Lan <evanl@marketamerica.com>
2014-06-10 14:39:20 +0800

    test

--- my-first-git-project/src/main/java/TestClass.java
+++ my-first-git-project/src/main/java/TestClass.java
@@ -2,7 +2,7 @@
 public class TestClass {
 	public void main(String[] aa){
 		System.out.println("Pull request test. #2");
-		System.out.println("Merged from Master");
+		System.out.println("Merged from Master!");
 		
 	}
 }
