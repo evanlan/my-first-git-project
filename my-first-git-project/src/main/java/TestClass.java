e40222a73041bcd74549cd85e7d73db44958aac6
Evan Lan <evanl@marketamerica.com>
2014-06-10 14:18:58 +0800

    merge for testing

--- my-first-git-project/src/main/java/TestClass.java
+++ my-first-git-project/src/main/java/TestClass.java
@@ -1,6 +1,7 @@
 
 public class TestClass {
 	public void main(String[] aa){
-		System.out.println("Pull request test.");
+		System.out.println("Pull request test. #2");
+		System.out.println("Merged from Master");
 	}
 }
